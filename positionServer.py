367baf88f840caa084531c74b1220ab707b38a4b
Robert Kathrein <robert@kathreins.at>
2019-09-04 11:52:56 +0200

    changed encoder word width,

--- positionServer.py
+++ positionServer.py
@@ -12,28 +12,39 @@ clear = lambda: os.system('cls')
 pivot_value = 0
 tilt_value  = 0
 running = True
-pull_rate_sleep_time = 0.0166666666666666666
+pull_rate_sleep_time = 1 / 55
 
 def readSerial():
     global pivot_value, tilt_value, running, pull_rate_sleep_time
+    
+    # Set encoder word width to 16 bit
+    pivot.write(b'B16\r')
+    tilt.write(b'B16\r')
     while running:
         pivot.write(b'?')
         tilt.write(b'?')
         time.sleep(pull_rate_sleep_time)
-        pivot_value = str(pivot.readline())[2:-3]
-        tilt_value = str(tilt.readline())[2:-3]
+        pivot_value = pivot.readline().decode('utf-8')
+        tilt_value = tilt.readline().decode('utf-8')
 
 def printSerial():
     global pivot_value, tilt_value, running
-    while running:
-        clear()
-        print("pivot: {}".format(pivot_value))
-        print("tilt: {}".format(tilt_value))
-        time.sleep(0.1)
+    
+    old_pivot = -1
+    old_tilt = -1
+    while running:    
+        if old_pivot != pivot_value or old_tilt != tilt_value:
+            old_pivot = pivot_value
+            old_tilt = tilt_value
+            clear()
+            print("pivot: {}".format(pivot_value))
+            print("tilt: {}".format(tilt_value))
+        time.sleep(pull_rate_sleep_time)
         
 x = threading.Thread(target=readSerial)
 x.start()
 
+# Uncomment to enable CLI output
 #y = threading.Thread(target=printSerial)
 #y.start()
 
@@ -42,13 +53,18 @@ async def emitSerialDataToWebSocket(websocket, path):
     global pivot_value, tilt_value, running, pull_rate_sleep_time
     print("Client Connected")
     try:
+        old_pivot = -1
+        old_tilt = -1
         while running:
-            await websocket.send('{{"pivot":{},"tilt":{}}}'.format(pivot_value, tilt_value))
+            if old_pivot != pivot_value or old_tilt != tilt_value:
+                old_pivot = pivot_value
+                old_tilt = tilt_value
+                await websocket.send('{{"pivot":{},"tilt":{}}}'.format(old_pivot, old_tilt))
             await asyncio.sleep(pull_rate_sleep_time)
     except:
         print("Client Disconnected")
 
-start_server = websockets.serve(emitSerialDataToWebSocket, "localhost", 8765)
+start_server = websockets.serve(emitSerialDataToWebSocket, "0.0.0.0", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 print("Server started")
